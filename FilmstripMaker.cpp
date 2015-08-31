0143bb1ff267657cf59c8215f129a58ec4df95a3
abaga129 <ethanepr@hotmail.com>
2015-08-30 21:07:48 -0500

    Made several important changes/Fixed transparency issue

--- FilmstripMaker.cpp
+++ FilmstripMaker.cpp
@@ -8,104 +8,155 @@
 //(primarily in audio plugins where I need to have a single image contain several frames)
 //
 
-//#include "stdafx.h"
 
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/stitching/stitcher.hpp"
 
 #include <iostream>
 #include <vector>
-#include <queue>
-//#include <Windows.h>
+#include <queue>  
 
 using namespace cv;
 using namespace std;
 
+/***************************************************************
+Called for automatic reading of Images from the present working
+directory. For this to work, the files must be numbered in 
+order. (1.png, 2.png, 3.png, 4.png, etc...)
+***************************************************************/
+void getImagesAuto(int pNumImages, queue<Mat> &images){
+	int startingIndex;
+	string fExt;
+
+	cout<<"Enter Number of the first image (if it's 0.png, enter 0): ";
+	cin >> startingIndex;
+	cout << "Enter file extension of the images: ";
+	cin >> fExt;
+
+	string fName;
+
+	for(int i = startingIndex; i < pNumImages + 1; i++){
+
+		fName = to_string(i);
+		fName += fExt;
+
+		cout << "Reading " << fName <<"...";
+
+		images.push(imread(fName, -1));
+		
+		if(images.empty()){
+			cout << "Error. Failed to open file.\n";
+		}
+		else
+			cout << " Succesfully loaded!\n";
+	}
+}
+
+
+/***************************************************************
+Called for manual input of file names. It accepts file names
+one at a time and then reads them and stores them in the
+image Queue
+***************************************************************/
+void getImagesManual(int pNumImages, queue<Mat> &images){
+		string fName;
+		for (int i = 0; i < pNumImages; i++){
+		cout << "\nEnter File name To Be Appended: ";
+		cin >> fName;
+		cout << endl;
+
+		cout << "File To Be Read: " << fName << endl;
+		images.push(imread(fName, -1));
+
+		if(images.back().empty()){
+			cout << "Error. Failed to open file.\n";
+			cout << "Try entering file name again.\n";
+			i--;
+		}
+		else
+			cout <<" Succesfully loaded!\n";
+
+
+	}
+}
+
+/***************************************************************
+Recieves the queue of Mats that was created by either 
+getImagesManual() or getImagesAuto() and combines them all
+into one Mat and then returns it.
+***************************************************************/
+Mat compileImages(queue<Mat> images){
+	Mat newMat;
+	while(!images.empty()){
+		newMat.push_back(images.front());
+		images.pop();
+	}	
+	return newMat;	
+}
+
+/***************************************************************
+Just a simple greeting with information about the program
+***************************************************************/
+void greeting(){
+	String version = "1.1";
+	cout << "\n\n************************************************\n"
+	     << "Filmstrip Maker v"<< version << '\n'
+	     << "Cut Through Recordings\n"
+	     << "This software is free under the GPL v3 License.\n"
+	     << "You are free to edit and redistribute this code\n"
+	     << "as you like.\n"
+	     << "************************************************\n";
+	cout << "\nThis program is used for combining two or more\n"
+		 << "images into a single strip of images. It is \n"
+		 << "useful for audio plugin guis, particually for\n"
+		 << "making meters.\n\n"
+		 << "If you have any questions or want to contribute,\n"
+		 << "you can email me at ctrecordingslive@gmail.com\n";
+}
+
 int main(int argc, const char** argv)
 {
+	greeting();
 	int numImages;
+	String outputFileName = "filmStrip.png";
 
-	String version = "1.0";
-	String file;
-	String outputFileName = "filmStrip.jpg";
-	String windowName = "Film Strip Maker v" + version;
-	//SetConsoleTitle(TEXT("Filmstrip Maker")); //Windows Only Code. Must Include Windows.h to use.
+	//Used for storing all the images
+	queue<Mat> imageVector;
 
-	/*
-	for (int i = 0; i < argc; i++){
-		cout << argv[i];
-		if (argv[i] == "-o" && argc > i){
-			outputFileName = argv[i + 1];
-		}
-	}*/
-
+	//All images will be stitched together and stored here
 	Mat finalImage;
 
 	cout << "\nEnter Number Of Images To Be Read: ";
 	cin >> numImages;
 	cout << endl;
 
+	cout << "Chose Mode:\n1.Auto Read: Reads files that already sorted by name, example(1.png,2.png,3.png..)";
+	cout << "\n2.Manual Read: asks for file names one at a time.\n: ";
+	int choice = 0;
+	cin >> choice;
 
-
-	for (int i = 0; i < numImages; i++){
-		cout << "Enter File name To Be Appended: ";
-		cin >> file;
-		cout << endl;
-
-		cout << "File To Be Read: " << file << endl;
-		Mat img = imread(file, -1);
-
-		if (img.empty()) //check whether the image is loaded or not
-		{
-			cout << "Error : Failed to load Image: " << file << endl
-			     << "Check spelling and try again..." << endl;
-			i = i - 1;
-
-		}
-
-		else
-		{
-			cout << file << " was succesfully loaded and added as Frame " << i + 1 << "." << endl << endl;
-			finalImage.push_back(img);
-		}
-
-
+	switch(choice){
+		//Auto Read
+		case 1: 
+			getImagesAuto(numImages, imageVector);
+			break;
+		//Manual Read
+		case 2:
+			getImagesManual(numImages, imageVector);
+			break;
+		//Error - Exit with code 1
+		default:
+			cout<<"\nError: Invalid choice.\n";
+			return 1;
 	}
 
+	//Takes all images from the image queue and stitches them to
+	//The final image
+	finalImage = compileImages(imageVector);
 
 	imwrite(outputFileName, finalImage);
 
 	cout << endl << "The Filmstrip was created and stored as " << outputFileName << endl << endl;
 
-	system("pause"); //Wait for a key to be pressed to exit.
-
-	//-----------Will display the Final image once it is processed
-	/*
-	namedWindow(windowName, CV_WINDOW_AUTOSIZE);
-	imshow(windowName, finalImage);
-
-
-	waitKey(0);
-	destroyWindow(windowName); //destroy the window with the name, "MyWindow"
-	*/
-
 	return 0;
 }
-
-/*
-
-void getImagesAuto(int startingIndex, int pNumImages, string fExt,  vector<Mat> images){
-	string fName;
-	for(int i = startingIndex; i < pNumImages; i++){
-		fname = i + fExt;
-		images.push_back(imread(fname, -1);
-		                if (img.empty()) //check whether the image is loaded or not
-                {
-                        cout << "Error : Failed to load Image: " << file << endl
-                             << "Check spelling and try again..." << endl;
-                        i = i - 1;
-
-                }
-	}
-}
-*/
